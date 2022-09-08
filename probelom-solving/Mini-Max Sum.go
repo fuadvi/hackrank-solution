4b6143c40029beb60852672cf46ea9af401c7127
fuad <fuad.pcd@gmail.com>
2022-09-08 22:20:22 +0700

    Mini-Max Sum

--- probelom-solving/Mini-Max Sum.go
+++ probelom-solving/Mini-Max Sum.go
@@ -2,36 +2,50 @@ package main
 
 import "fmt"
 
+// [1,2,3,4,5]
+// apabila hitung jumalah kecuali nilai min yang di dalam array
+// [2,3,4,5]
+
 func miniMaxSum(arr []int32) {
 	// Write your code here
 	var max, min int64
 
-	exc := arr[0]
-
-	for _, v := range arr {
-		if v < exc {
-			exc = v
+	for i := 0; i < len(arr); i++ {
+		// fmt.Print(arr[0])
+		for j := 0; j < len(arr)-1; j++ {
+			temp := arr[j]
+			if arr[j] > arr[j+1] {
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+			// fmt.Println(" ")
 		}
 	}
 
-	for i := 0; i < len(arr)-1; i++ {
+	for i := 1; i < len(arr); i++ {
 
 		min += int64(arr[i])
 	}
 
-	for j := 0; j < len(arr); j++ {
-		if int(exc) == int(arr[j]) {
-			fmt.Println("ss", arr[j])
-			continue
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			temp := arr[j]
+			if arr[j] < arr[j+1] {
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
 		}
+	}
+
+	for j := 1; j < len(arr); j++ {
 		max += int64(arr[j])
 	}
 
-	fmt.Println(min, max, exc)
+	fmt.Println(max, min)
 
 }
 
 func main() {
-	arr := []int32{7, 69, 2, 221, 8974}
+	arr := []int32{1, 2, 3, 4, 5}
 	miniMaxSum(arr)
 }
